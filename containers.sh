b625644db1ef74ad57ffb4dade77e858da602e4c
Isaac Caruso <44649197+icaruso21@users.noreply.github.com>
2020-11-10 13:30:14 -0800

    Update containers.sh

--- containers.sh
+++ containers.sh
@@ -10,8 +10,10 @@ do
 done
 
 echo "Spinning up RStudio server"
-sudo docker run -e PASSWORD=$passwd -e USER=$uname -d -p 8787:8787 -v /home/ec2-user/rstudio_shared/:/home/rstudio/rstudio_docker rocker/tidyverse
-echo "RStudio server is now online, connect in a browser at my_public_ip:8787"
+#sudo docker run -e PASSWORD=$passwd -e USER=$uname -d -p 8787:8787 -v /home/ec2-user/rstudio_shared/:/home/rstudio/rstudio_docker rocker/tidyverse
+sudo docker run -e PASSWORD=$passwd -e USER=$uname -d --expose 8787 -env "VIRTUAL_HOST=rstudio.trenchproject.com" --env "VIRTUAL_PORT=8787" --env "LETSENCRYPT_HOST=rstudio.trenchproject.com" --env "LETSENCRYPT_EMAIL=icaruso21@amherst.edu" -v /home/ec2-user/rstudio_shared/:/home/rstudio/rstudio_docker rocker/tidyverse
+
+echo "RStudio server is now online, connect in a browser at rstudio.trenchproject.com"
 echo "Shared filesystem is located at /home/ec2-user/rstudio_docker/"
 
 ls
@@ -24,7 +26,8 @@ echo "Building RShiny server"
 ls
 docker build -t shiny-server .
 echo "Running RShiny server"
-sudo docker run -d -p 3838:3838 -v /srv/shinyapps/:/srv/shiny-server/ -v /srv/shinylog/:/var/log/shiny-server/ shiny-server
+#sudo docker run -d -p 3838:3838 -v /srv/shinyapps/:/srv/shiny-server/ -v /srv/shinylog/:/var/log/shiny-server/ shiny-server
+sudo docker run -d --expose 3838 -env "VIRTUAL_HOST=map.trenchproject.com" --env "VIRTUAL_PORT=3838" --env "LETSENCRYPT_HOST=map.trenchproject.com" --env "LETSENCRYPT_EMAIL=icaruso21@amherst.edu" -v /srv/shinyapps/:/srv/shiny-server/ -v /srv/shinylog/:/var/log/shiny-server/ shiny-server
 
 # Pull apps from github (TO ADD MORE APPS: add a git pull line below for any additional repositories)
 cd /srv/shinyapps/
