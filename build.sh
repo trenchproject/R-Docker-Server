9fe6b5583ac06ba2110e4b6ecd97c4aea3786e4c
Isaac Caruso <44649197+icaruso21@users.noreply.github.com>
2020-11-09 18:56:49 -0800

    Update build.sh

--- build.sh
+++ build.sh
@@ -9,8 +9,9 @@ do
 done
 
 # Install, Build and Run Docker
-echo "Updating ec2 instance"
-sudo yum update
+echo "Updating ec2 instance and installing git"
+sudo yum update -y
+sudo yum install git -y
 
 echo "Installing Docker" 
 sudo amazon-linux-extras install docker
@@ -26,3 +27,5 @@ echo "Spinning up RStudio server"
 sudo docker run -e PASSWORD=$passwd -d -p 8787:8787 -v /home/ec2-user/rstudio_docker/:/home/rstudio/rstudio_docker rocker/tidyverse
 echo "RStudio server is now online, connect in a browser at my_public_ip:8787"
 echo "Shared filesystem is located at /home/ec2-user/rstudio_docker/"
+
+git pull https://github.com/trenchproject/Shiny-Docker-Server.git
