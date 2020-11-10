9628bfc24eca8e4a8c68b8390984e58ba07e1028
Isaac Caruso <44649197+icaruso21@users.noreply.github.com>
2020-11-10 11:26:41 -0800

    Update secure.sh

--- secure.sh
+++ secure.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
+# Install Docker Compose
+sudo curl -L "https://github.com/docker/compose/releases/download/1.27.4/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
+sudo ln -s /usr/local/bin/docker-compose /usr/bin/docker-compose
+docker-compose -v 
+
 mkdir data
 cd data
 mkdir nginx
