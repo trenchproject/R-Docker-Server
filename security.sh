b67f04b70025930aba8016ea55a18c3913c91ce9
Isaac Caruso <44649197+icaruso21@users.noreply.github.com>
2020-11-10 11:03:26 -0800

    Update security.sh

--- security.sh
+++ security.sh
@@ -7,15 +7,15 @@ sudo chmod +x /usr/local/bin/docker-compose
 mkdir data
 cd data
 mkdir nginx
+mkdir certbot
 cd ..
 cp ./R-Docker-Server/app.conf ./data/nginx/
 cp ./R-Docker-Server/docker-compose.yml ./data/nginx/
 cp ./R-Docker-Server/init-letsencrypt.sh ./data/nginx/
-cd ./data/nginx/
 
 # Change for relative path
-chmod +x init-letsencrypt.sh
-sudo ./init-letsencrypt.sh
+chmod +x ./data/nginx/init-letsencrypt.sh
+sudo ./data/nginx/init-letsencrypt.sh
 
 # Run NGINX and Let's Encrypt
 docker-compose up
