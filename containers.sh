9f593a89b7288307cb9990576a4dd5826a1f87d0
Isaac Caruso <44649197+icaruso21@users.noreply.github.com>
2020-11-09 22:23:54 -0800

    Create containers.sh

---
+++ containers.sh
@@ -0,0 +1,32 @@
+echo "Spinning up RStudio server"
+sudo docker run -e PASSWORD=$passwd -e USER=$uname -d -p 8787:8787 -v /home/ec2-user/rstudio_shared/:/home/rstudio/rstudio_docker rocker/tidyverse
+echo "RStudio server is now online, connect in a browser at my_public_ip:8787"
+echo "Shared filesystem is located at /home/ec2-user/rstudio_docker/"
+
+ls
+if [ -d "~/R-Docker-Server" ]; then 
+    git clone https://github.com/trenchproject/R-Docker-Server.git
+fi
+
+cd ~/R-Docker-Server
+echo "Building RShiny server"
+ls
+docker build -t shiny-server .
+echo "Running RShiny server"
+sudo docker run -d -p 80:3838 -v /srv/shinyapps/:/srv/shiny-server/ -v /srv/shinylog/:/var/log/shiny-server/ shiny-server
+
+# Pull apps from github (TO ADD MORE APPS: add a git pull line below for any additional repositories)
+cd /srv/shinyapps/
+git clone https://github.com/trenchproject/Climate-Change-Metabolism.git
+git clone https://github.com/icaruso21/Insect-Phenology-Forecaster.git
+git clone https://github.com/trenchproject/RShiny_robomussels.git
+git clone https://github.com/trenchproject/RShiny_Lizards.git
+echo "The following apps have been pulled to the shiny server"
+ls -l
+echo "Debug logs are available at /srv/shinylog/"
+echo "The following images are ready to be ran"
+docker images 
+echo "The following containers are currently running"
+docker ps 
+echo "more commands can be found using docker --help"
+
