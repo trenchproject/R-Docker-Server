97ccb95e0b0406d765ce884a782302ba0278ca00
Isaac Caruso <44649197+icaruso21@users.noreply.github.com>
2020-11-10 11:01:37 -0800

    Update security.sh

--- security.sh
+++ security.sh
@@ -4,6 +4,15 @@
 sudo curl -L "https://github.com/docker/compose/releases/download/1.27.4/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 sudo chmod +x /usr/local/bin/docker-compose
 
+mkdir data
+cd data
+mkdir nginx
+cd ..
+cp ./R-Docker-Server/app.conf ./data/nginx/
+cp ./R-Docker-Server/docker-compose.yml ./data/nginx/
+cp ./R-Docker-Server/init-letsencrypt.sh ./data/nginx/
+cd ./data/nginx/
+
 # Change for relative path
 chmod +x init-letsencrypt.sh
 sudo ./init-letsencrypt.sh
