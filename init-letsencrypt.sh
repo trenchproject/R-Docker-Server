f81d24174e951ba1c10c4de71eebdb72c273151d
Isaac Caruso <44649197+icaruso21@users.noreply.github.com>
2020-11-10 11:26:18 -0800

    Update init-letsencrypt.sh

--- init-letsencrypt.sh
+++ init-letsencrypt.sh
@@ -1,11 +1,5 @@
 #!/bin/bash
 
-
-# Install Docker Compose
-sudo curl -L "https://github.com/docker/compose/releases/download/1.27.4/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
-sudo chmod +x /usr/local/bin/docker-compose
-docker-compose -v 
-
 if ! [ -x "$(command -v docker-compose)" ]; then
   echo 'Error: docker-compose is not installed.' >&2
   exit 1
