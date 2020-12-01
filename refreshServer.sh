17ef66450db6eadc4aac3b34264ee6e33f794250
Isaac Caruso <44649197+icaruso21@users.noreply.github.com>
2020-12-01 11:02:11 -0800

    Update refreshServer.sh

--- refreshServer.sh
+++ refreshServer.sh
@@ -8,5 +8,6 @@ sudo git pull https://github.com/icaruso21/Insect-Phenology-Forecaster.git
 sudo git pull https://github.com/trenchproject/RShiny_robomussels.git
 sudo git pull https://github.com/trenchproject/RShiny_Lizards.git
 sudo git pull https://github.com/trenchproject/RShiny_BiophysicalModelMap.git
+sudo git pull https://github.com/trenchproject/RShiny_PlantPhenology
 
 touch mario.txt
