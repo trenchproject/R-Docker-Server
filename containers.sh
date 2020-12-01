31f692ca41ceba56c6e28c70903890f1ee7dfef7
Isaac Caruso <44649197+icaruso21@users.noreply.github.com>
2020-12-01 14:53:48 -0800

    Update containers.sh

--- containers.sh
+++ containers.sh
@@ -18,21 +18,13 @@ echo "###   Creating shared volume"
 
 
 # Pull apps from github (TO ADD MORE APPS: add a git pull line below for any additional repositories)
-cd ./R-Docker-Server/dat/
-#sudo git clone https://github.com/trenchproject/Climate-Change-Metabolism.git
-#sudo git clone https://github.com/icaruso21/Insect-Phenology-Forecaster.git
-#sudo git clone https://github.com/trenchproject/RShiny_robomussels.git
-#sudo git clone https://github.com/trenchproject/RShiny_Lizards.git
-#sudo git clone https://github.com/trenchproject/RShiny_BiophysicalModelMap.git
-#sudo git clone https://github.com/trenchproject/RShiny_PlantPhenology
-docker build -t data .
-docker run -name r-data data true
-
-cd /home/ec2-user/
+#cd ./R-Docker-Server/dat/
+#docker build -t data .
+#docker run -name r-data data true
 
 echo "###   Spinning up RStudio server"
 #sudo docker run -e PASSWORD=$passwd -e USER=$uname -d -p 8787:8787 -v /home/ec2-user/rstudio_shared/:/home/rstudio/rstudio_docker rocker/tidyverse
-sudo docker run  --name rstudio -e PASSWORD=$passwd -e USER=$uname -d --expose 8787 --env "VIRTUAL_HOST=rstudio.trenchproject.com" --env "VIRTUAL_PORT=8787" --env "LETSENCRYPT_HOST=rstudio.trenchproject.com" --env "LETSENCRYPT_EMAIL=icaruso21@amherst.edu" -volumes-from data -v /data:/home/rstudio/shiny_apps rocker/tidyverse
+sudo docker run  --name rstudio -e PASSWORD=$passwd -e USER=$uname -d --expose 8787 --env "VIRTUAL_HOST=rstudio.trenchproject.com" --env "VIRTUAL_PORT=8787" --env "LETSENCRYPT_HOST=rstudio.trenchproject.com" --env "LETSENCRYPT_EMAIL=icaruso21@amherst.edu" -volumes-from r-data -v /data:/home/rstudio/shiny_apps rocker/tidyverse
 
 echo "###   RStudio server is now online, connect in a browser at rstudio.trenchproject.com"
 echo "Shared filesystem is located at /home/ec2-user/RShared/"
@@ -48,8 +40,18 @@ ls
 docker build -t shiny-server .
 echo "###   Running RShiny server"
 #sudo docker run -d -p 3838:3838 -v /srv/shinyapps/:/srv/shiny-server/ -v /srv/shinylog/:/var/log/shiny-server/ shiny-server
-sudo docker run --name shiny -d --expose 3838 --env "VIRTUAL_HOST=map.trenchproject.com" --env "VIRTUAL_PORT=3838" --env "LETSENCRYPT_HOST=map.trenchproject.com" --env "LETSENCRYPT_EMAIL=icaruso21@amherst.edu" -volumes-from r-data -v ./data:/srv/shiny-server/ -v /srv/shinylog/:/var/log/shiny-server/ shiny-server
+sudo docker run --name shiny -d --expose 3838 --env "VIRTUAL_HOST=map.trenchproject.com" --env "VIRTUAL_PORT=3838" --env "LETSENCRYPT_HOST=map.trenchproject.com" --env "LETSENCRYPT_EMAIL=icaruso21@amherst.edu" -volumes-from r-data -v /data:/srv/shiny-server/ -v /srv/shinylog/:/var/log/shiny-server/ shiny-server
 
+cd ./RShared
+sudo git clone https://github.com/trenchproject/Climate-Change-Metabolism.git
+sudo git clone https://github.com/icaruso21/Insect-Phenology-Forecaster.git
+sudo git clone https://github.com/trenchproject/RShiny_robomussels.git
+sudo git clone https://github.com/trenchproject/RShiny_Lizards.git
+sudo git clone https://github.com/trenchproject/RShiny_BiophysicalModelMap.git
+sudo git clone https://github.com/trenchproject/RShiny_PlantPhenology
+
+
+cd ..
 
 echo "###   The following apps have been pulled to the shiny server"
 ls -l
