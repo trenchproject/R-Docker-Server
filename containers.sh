9b361d0db852dfb8b18f67de60959c96dadb82af
Isaac Caruso <44649197+icaruso21@users.noreply.github.com>
2020-12-02 13:20:49 -0800

    Update containers.sh

--- containers.sh
+++ containers.sh
@@ -24,7 +24,7 @@ echo "###   Creating shared volume"
 
 #echo "###   Spinning up RStudio server"
 #sudo docker run -e PASSWORD=$passwd -e USER=$uname -d -p 8787:8787 -v /home/ec2-user/rstudio_shared/:/home/rstudio/rstudio_docker rocker/tidyverse
-sudo docker run  --name rstudio -e PASSWORD=$passwd -e USER=$uname -d --expose 8787 --env "VIRTUAL_HOST=rstudio.trenchproject.com" --env "VIRTUAL_PORT=8787" --env "LETSENCRYPT_HOST=rstudio.trenchproject.com" --env "LETSENCRYPT_EMAIL=icaruso21@amherst.edu" -v /srv/shinyapps/:/home/rstudio/shiny_apps/ rocker/tidyverse
+sudo docker run  --name rstudio -e PASSWORD=$passwd -e USER=$uname -d --expose 8787 --env "VIRTUAL_HOST=rstudio.trenchproject.com" --env "VIRTUAL_PORT=8787" --env "LETSENCRYPT_HOST=rstudio.trenchproject.com" --env "LETSENCRYPT_EMAIL=icaruso21@amherst.edu" -v ~/shinyapps/:/home/rstudio/shiny_apps/ rocker/tidyverse
 
 echo "###   RStudio server is now online, connect in a browser at rstudio.trenchproject.com"
 echo "Shared filesystem is located at /srv/shinyapps"
@@ -40,10 +40,10 @@ ls
 docker build -t shiny-server .
 echo "###   Running RShiny server"
 #sudo docker run -d -p 3838:3838 -v /srv/shinyapps/:/srv/shiny-server/ -v /srv/shinylog/:/var/log/shiny-server/ shiny-server
-sudo docker run --name shiny -d --expose 3838 --env "VIRTUAL_HOST=map.trenchproject.com" --env "VIRTUAL_PORT=3838" --env "LETSENCRYPT_HOST=map.trenchproject.com" --env "LETSENCRYPT_EMAIL=icaruso21@amherst.edu" -v /srv/shinyapps/:/srv/shiny-server/ -v /srv/shinylog/:/var/log/shiny-server/ shiny-server # --volumes-from r-data
+sudo docker run --name shiny -d --expose 3838 --env "VIRTUAL_HOST=map.trenchproject.com" --env "VIRTUAL_PORT=3838" --env "LETSENCRYPT_HOST=map.trenchproject.com" --env "LETSENCRYPT_EMAIL=icaruso21@amherst.edu" -v ~/shinyapps/:/srv/shiny-server/ -v ~/shinylog/:/var/log/shiny-server/ shiny-server # --volumes-from r-data
 
 # Pull apps from github (TO ADD MORE APPS: add a git pull line below for any additional repositories)
-cd /srv/shinyapps/
+cd ~/shinyapps/
 sudo git clone https://github.com/trenchproject/Climate-Change-Metabolism.git
 sudo git clone https://github.com/icaruso21/Insect-Phenology-Forecaster.git
 sudo git clone https://github.com/trenchproject/RShiny_robomussels.git
@@ -54,7 +54,7 @@ cd ~/
 
 echo "###   The following apps have been pulled to the shiny server"
 ls -l
-echo "###   Debug logs are available at /srv/shinylog/"
+echo "###   Debug logs are available at ~/shinylog/"
 echo "###   The following images are ready to be ran"
 docker images 
 echo "###   The following containers are currently running"
