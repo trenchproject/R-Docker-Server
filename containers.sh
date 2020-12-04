9cd9f6d7bff57b0f72b0a6ce0f1aae69f0cc059e
Isaac Caruso <44649197+icaruso21@users.noreply.github.com>
2020-12-04 13:10:14 -0800

    Update containers.sh

--- containers.sh
+++ containers.sh
@@ -10,7 +10,7 @@ do
 done
 echo "###   Preparing reverse proxy, https, and certification for secure container deployment"
 docker run --detach --name nginx-proxy --publish 80:80 --publish 443:443 --volume /etc/nginx/certs --volume /etc/nginx/vhost.d --volume /usr/share/nginx/html --volume /var/run/docker.sock:/tmp/docker.sock:ro jwilder/nginx-proxy
-sudo docker run --detach --name nginx-proxy-letsencrypt --volumes-from nginx-proxy --volume /var/run/docker.sock:/var/run/docker.sock:ro --env "DEFAULT_EMAIL=icaruso21@amherst.edu" jrcs/letsencrypt-nginx-proxy-companion
+sudo docker run --detach --name nginx-proxy-letsencrypt --volumes-from nginx-proxy --volume /var/run/docker.sock:/var/run/docker.sock:ro --volume /etc/acme.sh --env "DEFAULT_EMAIL=icaruso21@amherst.edu" jrcs/letsencrypt-nginx-proxy-companion
 
 echo "###   Creating shared volume"
 #docker volume create --name RShared
