cfd4b1b9fb0fedb52ed88f9008fb5747fcbb1e0c
Isaac Caruso <44649197+icaruso21@users.noreply.github.com>
2020-12-04 13:52:32 -0800

    Update refreshServer.sh

--- refreshServer.sh
+++ refreshServer.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
 
 cd /srv/shinyapps/
+find . -type d -depth 1 -exec git --git-dir={}/.git --work-tree=$PWD/{} pull origin master \;
 
 # Github Repositories (of shiny apps) to pull 
-sudo git pull https://github.com/trenchproject/Climate-Change-Metabolism.git
-sudo git pull https://github.com/icaruso21/Insect-Phenology-Forecaster.git
-sudo git pull https://github.com/trenchproject/RShiny_robomussels.git
-sudo git pull https://github.com/trenchproject/RShiny_Lizards.git
-sudo git pull https://github.com/trenchproject/RShiny_BiophysicalModelMap.git
-sudo git pull https://github.com/trenchproject/RShiny_PlantPhenology
+#sudo git pull https://github.com/trenchproject/Climate-Change-Metabolism.git
+#sudo git pull https://github.com/icaruso21/Insect-Phenology-Forecaster.git
+#sudo git pull https://github.com/trenchproject/RShiny_robomussels.git
+#sudo git pull https://github.com/trenchproject/RShiny_Lizards.git
+#sudo git pull https://github.com/trenchproject/RShiny_BiophysicalModelMap.git
+#sudo git pull https://github.com/trenchproject/RShiny_PlantPhenology
 
 touch mario.txt
