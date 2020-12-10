352a109f7527e2d5b3afdd16adfab304d9e0c6b7
Isaac Caruso <44649197+icaruso21@users.noreply.github.com>
2020-12-10 14:58:45 -0800

    Update build.sh

--- build.sh
+++ build.sh
@@ -17,7 +17,7 @@ sudo yum install libcurl-devel -y
 sudo yum install libxml2-devel -y
 sudo yum install libjpeg-turbo-devel -y
 #sudo yum install gdal-devel -y
-#sudo dnf install gdal* expat* proj* -y
+sudo yum install gdal* expat* proj* -y
 echo "### Adding necessary C packages for R libraries (https://gist.github.com/abelcallejo/e75eb93d73db6f163b076d0232fc7d7e)" 
 
 
