7af63be7d4ba28edb1b7e7a3bcbeb6c22e11430d
Isaac Caruso <44649197+icaruso21@users.noreply.github.com>
2020-12-17 12:54:51 -0800

    Update containers.sh

--- containers.sh
+++ containers.sh
@@ -62,13 +62,19 @@ echo "###   The following containers are currently running"
 docker ps 
 echo "###    More commands can be found using docker --help"
 
+echo "###    Building Insect-Phenology updates"
 sudo cp /home/ec2-user/R-Docker-Server/isaacUpdates/Dockerfile /srv/shinyapps/Insect-Phenology-Forecaster/Dockerfile
 cd /srv/shinyapps/Insect-Phenology-Forecaster
-
 docker build -t isaac/updates .
 
+echo "###    Building Biophysical-Model-Map updates"
+sudo cp /home/ec2-user/R-Docker-Server/yutaroUpdates/Dockerfile /srv/shinyapps/RShiny_BiophysicalModelMap/Dockerfile
+cd /srv/shinyapps/RShiny_BiophysicalModelMap
+docker build -t yutaro/updates .
+
+echo "###    Adding update scripts to crontab"
 sudo chmod +x /home/ec2-user/R-Docker-Server/*.sh
 sudo cp /home/ec2-user/R-Docker-Server/refreshServer.sh /etc/cron.hourly/
-(crontab -l 2>/dev/null; echo "30 22 * * * Rscript '/srv/shinyapps/RShiny_BiophysicalModelMap/updates.R' >> '/var/log/yutaro_app_updates.log' 2>&1") | crontab -
 (crontab -l 2>/dev/null; echo "30 22 * * * docker run -i -t --rm -v /srv/shinyapps/Insect-Phenology-Forecaster:/code isaac/updates >> '/var/log/isaac_app_updates.log' 2>&1") | crontab -
+(crontab -l 2>/dev/null; echo "30 22 * * * docker run -i -t --rm -v /srv/shinyapps/RShiny_BiophysicalModelMap:/code yutaro/updates >> '/var/log/yutaro_app_updates.log' 2>&1") | crontab -
 
