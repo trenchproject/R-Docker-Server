bec696a270f6da0669a253d52dcf39889480c436
Isaac Caruso <44649197+icaruso21@users.noreply.github.com>
2020-12-22 13:12:15 -0800

    Update containers.sh

--- containers.sh
+++ containers.sh
@@ -78,5 +78,8 @@ sudo cp /home/ec2-user/R-Docker-Server/refreshServer.sh /etc/cron.hourly/
 (crontab -l 2>/dev/null; echo "30 23 * * * docker run -i -t --rm -v /srv/shinyapps/Insect-Phenology-Forecaster:/code isaac/updates >> '/var/log/isaac_app_updates.log' 2>&1") | crontab -
 (crontab -l 2>/dev/null; echo "00 02 * * * docker run -i -t --rm -v /srv/shinyapps/RShiny_BiophysicalModelMap:/code yutaro/updates >> '/var/log/yutaro_app_updates.log' 2>&1") | crontab -
 
+sudo chmod -R 777 /srv/shinyapps/RShiny_Microclim/Data/
+sudo chmod -R 777 /srv/shinyapps/Insect-Phenology-Forecaster/dat/
+
 aws s3 sync /srv/shinyapps/Insect-Phenology-Forecaster/dat/ s3://insect-phenology-map-shiny/ 
 aws s3 sync /srv/shinyapps/RShiny_Microclim/Data/ s3://microclim/
